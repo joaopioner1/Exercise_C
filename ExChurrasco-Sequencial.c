7eb63ae06a5d468c9a4c58f574d7c2473befd43b
Joao Vitor Souza Pioner <joaopioner123@gmail.com>
2020-08-11 22:33:49 -0300

    Apaguei comentários desnecessários.

--- ExChurrasco-Sequencial.c
+++ ExChurrasco-Sequencial.c
@@ -1,25 +1,3 @@
-/*#include <stdio.h>
-#include <math.h>
-int main()
-{
-    double base, altura;
-    
-    printf("Insira a base do retângulo: ");
-    scanf("%lf", &base);
-    printf("Insira a altura do retângulo: ");
-    scanf("%lf", &altura);
-   
-    double area = base * altura;
-    double perimetro = 2 * (base + base) + 2 * (altura + altura);
-    double diagonal = sqrt(pow(base, 2) + pow(altura, 2));
-    
-    printf("A area do retângulo é: %.4lf\n", area);
-    printf("O perímetro do retângulo é: %.4lf\n", perimetro);
-    printf("A diagonal do retângulo é: %.4lf\n", diagonal);
-   
-    return 0;
-}
-*/
 #include <stdio.h>
 
 int main()
