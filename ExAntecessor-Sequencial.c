ae7a50f2c926349aca20be9a537d0486acd36a30
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-15 23:31:50 -0300

    Adicionei o exercício antecessor sequencial.

---
+++ ExAntecessor-Sequencial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int input;
+    
+    printf("Insira um numero: ");
+    scanf("%d", &input);
+    
+    printf("O numero antecessor: %d", input - 1);
+
+    return 0;
+}
+/*Desenvolva o algoritmo de um programa onde o usuário irá informar um número
+inteiro e o programa deve calcular e exibir o número imediatamente antecessor ao
+número digitado pelo usuário.
+*/
