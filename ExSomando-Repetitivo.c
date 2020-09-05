c94d349ea49a9b85d244f310c8b197a93e422466
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-04 23:47:24 -0300

    Update ExSomando-Repetitivo.c

--- ExSomando-Repetitivo.c
+++ ExSomando-Repetitivo.c
@@ -5,7 +5,6 @@ int main()
     double input, soma = 0;
     
     printf("Insira quantos numeros quiser, no final sera imprimida a soma tota. \nPara parar o programa, digite 0. \nInsira um numero: ");
-    scanf("%lf", &input);
     while (input != 0) {
         printf("Insira um numero: ");
         scanf("%lf", &input);
