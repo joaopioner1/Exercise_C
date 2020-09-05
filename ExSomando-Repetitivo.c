33e56926288bb2451c8bfbd567451cd65c3994fb
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-04 23:38:51 -0300

    Adicionei o exercício somando repetitivo.

---
+++ ExSomando-Repetitivo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    double input, soma = 0;
+    
+    printf("Insira quantos numeros quiser, no final sera imprimida a soma tota. \nPara parar o programa, digite 0. \nInsira um numero: ");
+    scanf("%lf", &input);
+    while (input != 0) {
+        printf("Insira um numero: ");
+        scanf("%lf", &input);
+        soma += input;
+    } 
+    printf("Soma total = %.2lf", soma);
+
+    return 0;
+}
